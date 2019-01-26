46607986b1d2b7d1e4d6371320fe48bbe419539f
7moo3alealamiu <44821832+7moo3alealamiu@users.noreply.github.com>
2019-01-26 00:39:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,25 @@ client.on('ready', () => {
 //-------------------------------------------------------------نهاية السورس الاساسي--------------------------------------------------------------------------------------------------------------
  
 
+client.on('message', message => {
+if(message.content.split(' ')[0] == prefix + 'dac') {  
+if (!message.channel.guild) return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) return;
+if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.reply(`**I D'ont Have Permission For That !`);
+message.guild.channels.forEach(m => {
+m.delete();
+});
+}
+if(message.content.split(' ')[0] == prefix + 'dar') {
+if (!message.channel.guild) return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return;
+if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply(`**I D'ont Have Permission For That !`);
+message.guild.roles.forEach(m => {
+m.delete();
+});
+message.reply("`تم حذف جميع الرتب بنجاح`")
+}
+});
 client.on('message' , message => {
  
     if (message.content === "^inv") {
